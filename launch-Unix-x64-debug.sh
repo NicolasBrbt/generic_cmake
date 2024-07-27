8e9094b7ecbe631ba6853d644e81a2c1a33ba9cc
NicolasBrbt <nicolastheo.barbut@edu.ece.fr>
2024-07-27 16:55:27 +0200

    Modification de la saisie de l'executable a executer

--- launch-Unix-x64-debug.sh
+++ launch-Unix-x64-debug.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
+# This script is used to run the executable generated with the Unix-x64-debug configuration
+# Give the name of your executable
+executable_name=""
 
 # Get the name of the current directory
 CURRENT_DIR=$(basename "$(pwd)")
 
 # Define the path to the executable using the parent directory
-exePath="../${CURRENT_DIR}-Unix-x64-debug/bin/main"
+exePath="../${CURRENT_DIR}-Unix-x64-debug/bin/${executable_name}"
 
 # Check if the executable exists and run it, or print an error message
 if [ -f "$exePath" ]; then
     "$exePath"
 else
-    echo "L'exécutable \"$exePath\" n'existe pas."
+    echo "The executable \"$exePath\" does not exist."
 fi
--- launch-mingw-x64-debug.bat
+++ launch-mingw-x64-debug.bat
@@ -1,11 +1,15 @@
 @echo off
+REM This script is used to launch the executable built with mingw-x64-debug
+REM Give the name of the executable
+set EXECUTABLE_NAME= "coucou"
 REM Get the name of the current directory
 for %%I in (.) do set "CURRENT_DIR=%%~nxI"
 
+
 REM Définir le chemin vers l'exécutable en utilisant le répertoire parent
-set "exePath=..\%CURRENT_DIR%-mingw-x64-debug\bin\main.exe"
+set "exePath=..\%CURRENT_DIR%-mingw-x64-debug\bin\%EXECUTABLE_NAME%.exe"
 if exist "%exePath%" (
     "%exePath%"
 ) else (
-    echo L'executable "%exePath%" n'existe pas.
+    echo The executable "%exePath%" does not exist.
 )
